abb7e1f0f59211b0a46ff47c3fd05cb3a056bdc6
Andrew Jones <ajones@digitalbazaar.com>
2024-10-09 14:16:40 -0400

    Add Data Integrity V2 Context to contexts.

--- documentLoader.js
+++ documentLoader.js
@@ -4,6 +4,7 @@ import { vcv2 } from './contexts/credv2.js';
 import { examplesv2 } from './contexts/examples-v2.js';
 import { vcv1 } from './contexts/credv1.js';
 import { edv1 } from './contexts/ed25519-signature-2020-v1.js';
+import { diV2 } from './contexts/data-integrity-v2.js';
 
 
 // Set up a document loader so we don't have to go to the net
@@ -12,7 +13,8 @@ const CONTEXTS = {
     "https://www.w3.org/ns/credentials/examples/v2": { "@context": examplesv2 },
     "https://www.w3.org/2018/credentials/v1": { "@context": vcv1 },
     "https://w3id.org/citizenship/v1": { "@context": citizenv1 },
-    "https://w3id.org/security/suites/ed25519-2020/v1": { "@context": edv1 }
+    "https://w3id.org/security/suites/ed25519-2020/v1": { "@context": edv1 },
+    "https://w3id.org/security/data-integrity/v2": {"@context": diV2 }
 };
 // Only needed if you want remote loading, see comments below
 const nodeDocumentLoader = jsonld.documentLoaders.node();
